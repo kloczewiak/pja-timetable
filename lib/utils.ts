597181a4770d29b05a2421459170fa3467eac2a7
MafinBoy <kloczewiak.filip@gmail.com>
2024-11-28 01:14:28 +0100

    Add groupBy function to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function groupBy<
+  T,
+  K extends keyof T,
+  KeyType extends Extract<T[K], string | number | symbol>,
+>(items: T[], key: K): Record<KeyType, Omit<T, K>[]> {
+  return items.reduce(
+    (result, item) => {
+      const group = item[key] as KeyType;
+      if (!result[group]) {
+        result[group] = [];
+      }
+      const { [key]: _, ...rest } = item;
+      result[group].push(rest);
+      return result;
+    },
+    {} as Record<KeyType, Omit<T, K>[]>,
+  );
+}
